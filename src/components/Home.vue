3fe1b5411bc58206018610cbe1d9bd614374a8e8
Vannes Wijaya <vannesoey@gmail.com>
2021-08-08 21:17:06 +0700

    home intro

---
+++ src/components/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="overflow-hidden">
+    <section class="bg-birutua w-screen h-screen flex">
+      <div class="flex flex-nowrap fixed">
+        <div class="w-screen flex flex-col justify-center pl-32">
+          <div ref="hey" class="font-black text-kuning text-9xl">Hey,</div>
+          <div ref="letMe" class="font-black text-kuning text-9xl pt-10">
+            Let me show you around.
+          </div>
+        </div>
+        <div ref="intro" class="w-screen h-screen grid grid-cols-2 py-20">
+          <div class="grid col-span-1">
+            <div class="font-black text-kuning text-9xl uppercase">
+              Nice to meet you
+            </div>
+            <div class="font-medium text-kuning text-7xl pt-10">
+              I'm Vannes, a software engineer and a computer science student.
+            </div>
+          </div>
+          <div class="grid col-span-1 pt-20">
+            <div
+              class="w-128 h-128 rounded-full bg-cover bg-center"
+              style="
+                background-image: url('https://i.ibb.co/K5q5z59/IMG-0555.jpg');
+              "
+            ></div>
+            <!-- <img src="../assets/logo.png" alt="" /> -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-birutua h-screen"></section>
+    <section class="bg-birutua h-screen"></section>
+    <section class="bg-birutua h-screen"></section>
+    <section class="bg-merah h-screen"></section>
+    <!-- <section class="bg-birutua w-screen h-screen p-20 bg-fixed">
+      <div class="font-black text-kuning text-9xl uppercase">
+        Nice to meet you
+      </div>
+      <div class="font-medium text-kuning text-7xl pt-20">
+        I'm Vannes, a software engineer and a computer science student.
+      </div>
+    </section> -->
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from "vue";
+
+export default {
+  setup() {
+    const hey = ref(null);
+    const letMe = ref(null);
+    const intro = ref(null);
+
+    onMounted(() => {
+      document.addEventListener("scroll", handleScroll);
+    });
+
+    onUnmounted(() => {
+      document.removeEventListener("scroll", handleScroll);
+    });
+
+    const handleScroll = (evt) => {
+      const scrollY = window.scrollY;
+
+      hey.value.style.transform = "translateY(" + -scrollY + "px)";
+      letMe.value.style.transform = "translateY(" + scrollY + "px)";
+      intro.value.style.transform = "translateX(" + -scrollY + "px)";
+    };
+
+    return {
+      hey,
+      letMe,
+      intro,
+    };
+  },
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      transitionProperty: ["hover", "focus"],
+      spacing: {
+        90: "22.5rem",
+        112: "28rem",
+        128: "32rem",
+        160: "40rem",
+      },
+      colors: {
+        merah: "#ef6461",
+        kuning: "#ffc43d",
+        birutua: "#2364aa",
+        birumuda: "#9cf6f6",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      padding: ["hover", "group-hover", "even", "odd"],
+      height: ["hover", "group-hover"],
+      width: ["hover", "group-hover"],
+      margin: ["hover", "group-hover"],
+      borderWidth: ["hover", "group-hover"],
+      fontWeight: ["hover", "group-hover"],
+      transform: ["hover", "group-hover"],
+      scale: ["hover", "group-hover"],
+      translate: ["hover", "group-hover"],
+      inset: ["hover", "group-hover"],
+      space: ["hover", "group-hover"],
+      transitionDelay: ["hover", "group-hover"],
+      backgroundColor: ["even", "odd", "active"],
+      flexDirection: ["even", "odd"],
+      textColor: ["even", "odd"],
+      backgroundImage: ["first", "last", "even", "odd"],
+      gradientColorStops: ["first", "last", "even", "odd"],
+    },
+  },
+  plugins: [],
+};
