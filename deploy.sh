1b604f8a9eb6e3be9397a4469376f0d64568a34d
Vannes Wijaya <vannesoey@gmail.com>
2021-08-10 01:11:03 +0700

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:vanneswijaya/vanneswijaya.github.io.git main
+git push -f git@github.com:vanneswijaya/vanneswijaya.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:vanneswijaya/archio.git master:gh-pages
